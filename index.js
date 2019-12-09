c2fd481baa6a9817f6a23f2022311b351c9e5ea5
WNortier <wnortier@gmail.com>
2019-12-09 11:49:47 +0200

    favorites feature for filter widget

--- index.js
+++ index.js
@@ -11,36 +11,116 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // parse application/json
 app.use(bodyParser.json());
 
+const pg = require("pg");
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://warwick:pg123@localhost:5432/carsworkshop';
+
+let useSSL = false;
+let local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
+const Pool = pg.Pool;
+const pool = new Pool({
+    connectionString,
+    ssl: useSSL
+});
+
 
 let brandList = ["Nissan", "Toyota"]
 
-app.post("/api/brand_add", function(req, res){
-    console.log(req.body.brand_name)
+
+
+
+
+
+
+
+
+
+
+
+app.post("/api/store_favorites", async (req, res, next) => {
+    //console.log(favorites)
+    try {
+        // await pool.query(`DELETE FROM carfavorites`)
+        let favorites = req.body.data
+        for (var i = 0; i < favorites.length; i++) {
+            await pool.query(`INSERT INTO carfavorites (favorites) VALUES ($1)`, [favorites[i]])
+        }
+        res.json({
+            status : "success"
+        })
+    } catch (err) {
+        // alert(err);
+        console.log(err)
+        res.json({
+            status: "error"
+    
+        })
+    }
+});
+
+app.get('/api/favorites', async (req, res, next) => {
+    try {
+        let theFavorites = []
+        let favoritesExtraction = await pool.query(`SELECT favorites from carfavorites`)
+        if (favoritesExtraction.rowCount !== 0) {
+            let theFavorites = favoritesExtraction.rows
+            res.json(theFavorites)
+        } else {
+            res.json(theFavorites);
+        }
+
+    } catch (err) {
+
+        console.log(err)
+        res.json({
+            status: "error"
+    
+        })
+        // alert(err);
+    }
+})
+
+
+
+
+
+
+
+
+app.post("/api/brand_add", function (req, res) {
+    //console.log(req.body.brand_name)
     brandList.push(req.body.brand_name)
     res.json({
-        status:"success"
+        status: "success"
     })
 })
 
-app.get('/api/brand_list', function(req, res){
+app.get('/api/brand_list', function (req, res) {
     res.json(brandList)
 });
 
-app.post("/api/brands_delete", function(req, res){
+app.post("/api/brands_delete", function (req, res) {
     brandList = []
     res.json({
-        status:"success"
+        status: "success"
     })
 })
 
--- public/filter.js
+++ public/filter.js
@@ -15,8 +15,55 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const colorSelect = document.querySelector('.colorSelect')
     const makeSelect = document.querySelector('.makeSelect')
-
     const allBtn = document.querySelector('.allBtn')
+
+    const showFavoritesBtn = document.querySelector('.showFavoritesBtn')
+    const storeFavoritesBtn = document.querySelector('.storeFavoritesBtn')
+
+    const errorTemplateInsertPoint = document.querySelector('.errorTemplateInsertPoint')
+    const errorTemplate = document.querySelector('.errorTemplate')
+    const errorTemplateInstance = Handlebars.compile(errorTemplate.innerHTML)
+
+    const dynamicDiv = document.querySelector(".dynamic")
+
+    // storeFavoritesBtn.
+
+
+
+    dynamicDiv.addEventListener("click", (evt) => {
+
+        const selected = Array.from(document.querySelectorAll(".carFavorite:checked"));
+        selectedFavorites = selected.map(e => e.value)
+        console.log(selectedFavorites)
+        axios.post('/api/store_favorites', {
+            data: selectedFavorites
+        })
+    })
+
+
+    showFavoritesBtn.addEventListener("click", () => {
+             axios.get('/api/favorites')
+            .then(function (res) {
+                var favorites = res.data.map((entry) => {
+                    return entry.favorites;
+                })
+                axios.get('http://api-tutor.herokuapp.com/v1/cars')
+                    .then(function (res) {
+                        let cars = res.data
+                        let result = cars.filter((car) => favorites.includes(car.reg_number));
+                        carsTemplateInsertPoint.innerHTML = carsTemplateInstance({
+                            car: result
+                        })
+                    });
+            });
+    });
+
+
+
+
+
+
+
 
     axios.get('http://api-tutor.herokuapp.com/v1/colors')
         .then(function (res) {
@@ -24,16 +71,26 @@ document.addEventListener("DOMContentLoaded", () => {
             //console.log(res.data)
             let colors = res.data
             colors.unshift("Select Color");
-            colorTemplateInsertPoint.innerHTML = colorListTemplate({
+            const colorsHtml = colorListTemplate({
                 color: colors
             });
 
+            colorTemplateInsertPoint.innerHTML = colorsHtml
+
         });
+
+
+
+
+
+
+
+
 
     axios.get('http://api-tutor.herokuapp.com/v1/makes')
         .then(function (res) {
             let makes = res.data
-           // console.log(makes)
+            // console.log(makes)
             makes.unshift("Select Make");
             const makesHtml = makeListTemplate({
                 make: makes
@@ -50,75 +107,64 @@ document.addEventListener("DOMContentLoaded", () => {
             })
         });
 
-        allBtn.addEventListener('click', () => {
-            carsTemplateInsertPoint.innerHTML = ""
-            axios.get('http://api-tutor.herokuapp.com/v1/cars')
+    allBtn.addEventListener('click', () => {
+        carsTemplateInsertPoint.innerHTML = ""
+        axios.get('http://api-tutor.herokuapp.com/v1/cars')
             .then(function (res) {
                 let cars = res.data
                 carsTemplateInsertPoint.innerHTML = carsTemplateInstance({
                     car: cars
                 })
             });
-    
-        })
+
+    })
 
     filterBtn.addEventListener('click', () => {
         let color = colorSelect.value
         let make = makeSelect.value
         carsTemplateInsertPoint.innerHTML = ""
 
-
         if (color && make == "Select Make") {
-            
             axios.get('http://api-tutor.herokuapp.com/v1/cars/color/' + color)
                 .then(function (res) {
-                    // handle success
-                    //console.log(res.data)
                     let selectedColorCars = res.data
-                    console.log(selectedColorCars)
-
-
                     carsTemplateInsertPoint.innerHTML = carsTemplateInstance({
                         car: selectedColorCars
                     });
 
                 });
-
-
-
         } else if (make && color == "Select Color") {
             carsTemplateInsertPoint.innerHTML = ""
             axios.get('http://api-tutor.herokuapp.com/v1/cars/make/' + make)
                 .then(function (res) {
-                    // handle success
-                    //console.log(res.data)
                     let selectedColorMakes = res.data
-
                     carsTemplateInsertPoint.innerHTML = carsTemplateInstance({
                         car: selectedColorMakes
                     });
-
                 });
-        } else if (color !== "Select Color" && make !== "Select Make"){
+        } else if (color !== "Select Color" && make !== "Select Make") {
             carsTemplateInsertPoint.innerHTML = ""
             axios.get('http://api-tutor.herokuapp.com/v1/cars/make/' + make + '/color/' + color)
                 .then(function (res) {
-                    // handle success
-                    //console.log(res.data)
                     let selectedColorAndMake = res.data
-
-
                     carsTemplateInsertPoint.innerHTML = carsTemplateInstance({
                         car: selectedColorAndMake
                     });
-
                 });
         }
     });
 
-
-
-   
-
-
 })
+
+
+//console.log(selectedFavorites)
+// else {
+// var errorMessage = [];
+//     errorMessage.push("No more than five favorites may be selected")
+//     var unchecker = document.querySelectorAll(".carFavorite:checked")
+//     //console.log(unchecker.length)
+//     //evt.target.checked=false
+//     errorTemplateInsertPoint.innerHTML = errorTemplateInstance({
+//         error: errorMessage
+//     })
+// }
